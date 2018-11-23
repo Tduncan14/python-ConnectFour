f4cf2d60228f3533b93ed18cc7bfe68326f95293
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-11-22 20:50:40 -0600

    keep moving forward

--- connect.py
+++ connect.py
@@ -1,3 +1,10 @@
 import numpy as np
 
+# function to create the board
 def create_board():
+    board = np.zeros((6,7))
+    return board
+
+#now im printing the board
+board = create_board()
+print(board)
